a9f59abaf119455608d0d5e333f8a389485431ca
Aaryash-Shakya <aaryashshakya.dev@gmail.com>
2025-08-29 09:27:04 +0545

    add: api caching and prerender for vercel

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -17,8 +17,15 @@ export default defineNuxtConfig({
 	vite: {
 		plugins: [tailwindcss()],
 	},
+	ssr: true,
 	nitro: {
 		preset: "vercel",
+		prerender: {
+			routes: ["/"], // add all static routes here, dynamic routes need to be listed explicitly
+		},
+		routeRules: {
+			"/api/**": { cache: { maxAge: 300 } }, // cache API responses for 5 minutes
+		},
 	},
 	modules: ["@nuxt/eslint", "@nuxt/icon", "@nuxt/fonts", "@nuxt/image", "@nuxt/ui"],
 });
